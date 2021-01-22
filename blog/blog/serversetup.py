2eaac0e469521dcb478685b57d8b210fade0c59a
Gesiz <1040749858@qq.com>
2021-01-22 21:46:25 +0800

    新建我的博客 配置基本设置

---
+++ blog/blog/serversetup.py
@@ -0,0 +1,30 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '42.193.187.243',  # 数据库主机
+        'PORT': 8306,  # 数据库端口
+        'USER': 'blogdjango',  # 数据库用户名
+        'PASSWORD': 'heimapeixun666',  # 数据库用户密码
+        'NAME': 'blog'  # 数据库名字
+    },
+}
+
+CACHES = {
+    "default": {  # 默认
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://42.193.187.243:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {  # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://42.193.187.243:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
